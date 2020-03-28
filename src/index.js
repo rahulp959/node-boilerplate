f46aaeb17361fbc0b2c1b1f71948acc6d08131ac
Rahul Parkar <rahul.a.parkar@gmail.com>
2020-03-28 17:47:20 +0000

    Make an error happen

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://3333a73f1d6544b0926f10bd5835c690@sentry.io/5178520' });
 
 export async function handler(event, context) {
-  console.log(Sentry)
-
   console.log(`EVENT: \n${  JSON.stringify(event, null, 2)}`)
+  console.log(event.bad.bad.bad)
   return context.logStreamName
 }
