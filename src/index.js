7ac30cfc2547f178d85aa98634846bb75c5f96c2
Rahul Parkar <rahul.a.parkar@gmail.com>
2020-03-28 17:41:03 +0000

    Add sentry init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import Sentry from '@sentry/node';
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://3333a73f1d6544b0926f10bd5835c690@sentry.io/5178520' });
 
 export async function handler(event, context) {
-  Sentry.init({ dsn: 'https://3333a73f1d6544b0926f10bd5835c690@sentry.io/5178520' });
-  
+  console.log(Sentry)
+
   console.log(`EVENT: \n${  JSON.stringify(event, null, 2)}`)
   return context.logStreamName
 }
